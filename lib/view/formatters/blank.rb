8e1fe13b52f2119fceafce5b3dd37443b92e3709
Iain Hecker <iain@iain.nl>
2010-10-10 18:16:06 +0200

    Documentation for blank

--- lib/view/formatters/blank.rb
+++ lib/view/formatters/blank.rb
@@ -1,9 +1,18 @@
 module View
 
+  # The default formatter for blank objects, like nil, empty strings and empty
+  # arrays. It will display an empty string or something else, which you can
+  # configure with I18n.
+  #
+  # @example config/locales/en.yml
+  #   en:
+  #     view:
+  #       blank: "nothing"
+  #
   class Blank < Formatter
 
     def format
-      ::I18n.t(:blank, :scope => :view, :default => "")
+      ::I18n.t(:blank, :scope => :view, :default => "").html_safe
     end
 
   end
