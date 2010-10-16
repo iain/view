bbd79f855bdcfc41a17fcb3a83dc8ac617e704ff
Iain Hecker <iain@iain.nl>
2010-10-16 22:59:21 +0200

    Add more helper methods, for inherited_resources too

--- lib/view/helper.rb
+++ lib/view/helper.rb
@@ -2,8 +2,35 @@ module View
 
   module Helper
 
+    # Use the formatter straight from the view
     def view(value, options = {}, &block)
       ::View.format(value, options, self, &block)
+    end
+
+    # Shortcut for rendering tables
+    def table_for(value, options = {}, &block)
+      view(value, options.merge(:as => :table), &block)
+    end
+
+    # If you're using something like InheritedResource, this will automatically
+    # set the table for the current collection, so you'll only need to do:
+    #
+    #   = table
+    def table(options = {}, &block)
+      table_for(collection, options.merge(:class => resource_class), &block)
+    end
+
+    # Shortcut for definition_lists
+    def definition_list_for(value, options = {}, &block)
+      view(value, options.merge(:as => :definition_list), &block)
+    end
+
+    # If you're using something like InheritedResource, this will automatically
+    # set the definition_list for the current resource, so you'll only need to do:
+    #
+    #   = definition_list
+    def definition_list(options = {}, &block)
+      definition_list_for(resource, options, &block)
     end
 
   end
--- spec/view/helper_spec.rb
+++ spec/view/helper_spec.rb
@@ -2,10 +2,32 @@ require 'spec_helper'
 
 describe "View::Helper" do
 
-  pending "views" do
-    template = Class.new do
-      include View::Helper
-    end
+  it "views" do
+    View.should_receive(:format).with('x', {:foo => 'bar'}, helper)
+    helper.view('x', :foo => 'bar')
+  end
+
+  it "renders a table" do
+    View.should_receive(:format).with('x', {:as => :table}, helper)
+    helper.table_for('x')
+  end
+
+  it "renders a definition list" do
+    View.should_receive(:format).with('x', {:as => :definition_list}, helper)
+    helper.definition_list_for('x')
+  end
+
+  it "renders a definition list for a resource" do
+    helper.should_receive(:resource).and_return('x')
+    View.should_receive(:format).with('x', {:as => :definition_list}, helper)
+    helper.definition_list
+  end
+
+  it "renders a definition list for a resource" do
+    helper.should_receive(:collection).and_return(['x'])
+    helper.should_receive(:resource_class).and_return(String)
+    View.should_receive(:format).with(['x'], {:as => :table, :class => String}, helper)
+    helper.table
   end
 
 end
