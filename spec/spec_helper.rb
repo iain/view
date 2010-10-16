ca4dbaaed325fb98635be93a6f2f2a6a2b670f6a
Iain Hecker <iain@iain.nl>
2010-10-16 13:12:27 +0200

    Add some documentation to understand the spec helper more

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,35 @@
 require "rubygems"
 require "bundler/setup"
 Bundler.setup :default
+
 require 'rails'
 require 'action_view'
+
+# Needed to make the capture helper work in tests
 require 'action_view/template/handlers/erb'
+
 require 'rspec'
 require 'view'
 
 module WithTranslation
+
+  # Temporarily use a clean 'test'-locale
+  #
+  # with_translation :foo => 'bar' do
+  #   I18n.t(:foo).should == 'bar'
+  # end
+  # I18n.t(:foo).should_not == 'bar'
   def with_translation(keys)
     locale = I18n.locale
-    I18n.backend.store_translations(:test, keys)
     I18n.locale = :test
+    I18n.backend.reload!
+    I18n.backend.store_translations(:test, keys)
     yield
   ensure
+    I18n.backend.reload!
     I18n.locale = locale
   end
+
 end
 
 
@@ -26,6 +40,7 @@ module TemplateHelper
     @helper ||= Template.new
   end
 
+  # emulate a Rails view
   class Template < ActionView::Base
     include View::Helper
   end
