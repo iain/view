fdf49007042ae0469b3178446b7a390be49a9b07
Iain Hecker <iain@iain.nl>
2010-10-17 12:44:19 +0200

    Specs for size formatter

--- lib/view/formatters/size.rb
+++ lib/view/formatters/size.rb
@@ -1,5 +1,12 @@
 module View
 
+  # Formats with number_to_human_size
+  #
+  # @example
+  #
+  #   = view 1999, :as => :size # => "1.95 KB"
+  #
+  # @see http://rubydoc.info/docs/rails/3.0.0/ActionView/Helpers/NumberHelper:number_to_human_size
   class Size < Formatter
 
     self.allowed_options = [ :precision, :separator, :delimiter ]
